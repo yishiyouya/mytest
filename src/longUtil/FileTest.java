36b6a5121da9d666e64e0068d5cef40ed14856a5
1742468256@qq.com <1742468256@qq.com>
2018-08-07 10:57:02 +0800

    file attr

--- src/longUtil/FileTest.java
+++ src/longUtil/FileTest.java
@@ -1,8 +1,17 @@
 package longUtil;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.pactera.sms.foundbase.util.GsdsCommonTool;
 
 public class FileTest {
 	
@@ -10,6 +19,27 @@ public class FileTest {
 		fpathExist();
 	}
 	
+	public static String fileCreTime(String fullFileName) {
+		Path path = Paths.get(fullFileName);  
+		String fileCreTime = "";
+		try {
+			if(path.toFile().exists()) {
+				BasicFileAttributeView basicview = Files.getFileAttributeView(path, BasicFileAttributeView.class,LinkOption.NOFOLLOW_LINKS );
+				BasicFileAttributes attr;
+				attr = basicview.readAttributes();
+				Date createDate = new Date(attr.creationTime().toMillis());
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String dateString = formatter.format(createDate);
+				Timestamp time = Timestamp.valueOf(dateString);
+				fileCreTime = time.toString();
+			}
+			fileCreTime = fileCreTime.replaceAll(":", "").replaceAll(" ", "_");
+			fileCreTime = fileCreTime.substring(0, fileCreTime.length() - 2);
+		} catch (Exception e) {
+			System.out.println(GsdsCommonTool.getStackInfo(e));
+		}
+        return fileCreTime;
+	}
 	public static void fpathExist() {
 		String fName = "E:\\DownLoad\\WeChat Files\\badfoxcc\\Files\\result.js";
 		Path path = Paths.get(fName);
