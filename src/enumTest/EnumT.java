a60392653e3565ac881277f40158f41651753eac
1742468256@qq.com <1742468256@qq.com>
2018-08-21 16:24:13 +0800

    enum

--- src/enumTest/EnumT.java
+++ src/enumTest/EnumT.java
@@ -1,7 +1,24 @@
 package enumTest;
 
-public enum EnumT {
-	H,
-	E,
-	O
+public enum EnumT{
+	H("h"),
+	E("e"),
+	O("o");
+	private String v;
+	private EnumT(String v) {
+		this.v = v;
+	}
+	public String getV() {
+		return v;
+	}
+	
+	public static String existV(String str) {
+		for(EnumT eT : EnumT.values()) {
+			if(eT.getV().equals(str)) {
+				return str;
+			}
+		}
+		return null;
+	}
+	
 }
--- src/enumTest/TestEnum.java
+++ src/enumTest/TestEnum.java
@@ -4,9 +4,15 @@ public class TestEnum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		valueEnum();
+		existEnum();
 	}
 
+	public static void existEnum() {
+		String string = "y";
+		String res = EnumT.existV(string);
+		System.out.println(res);
+	}
+	
 	public static void valueEnum() {
 		for(EnumT eString : EnumT.values()) {
 			System.out.println(eString);
